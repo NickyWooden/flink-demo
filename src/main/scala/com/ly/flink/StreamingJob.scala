c5cc21b4e54d9f16ef29fdcad19c14065968d9be
NickyWooden <1561386410@qq.com>
2019-09-19 19:55:28 +0800

    kafka 基站4G数据流 关联postgre中重点人进行实时预警，通过广播状态实现

--- src/main/scala/com/ly/flink/StreamingJob.scala
+++ src/main/scala/com/ly/flink/StreamingJob.scala
@@ -1,63 +1,126 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 package com.ly.flink
 
-import org.apache.flink.streaming.api.scala._
+import java.util.Properties
 
+import com.alibaba.fastjson.JSON
+import com.ly.flink.consts.CaseClass.{CrimeDoc, Phone, Warn}
+import com.ly.flink.job.sources.PostgreSource
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.api.common.state.{MapStateDescriptor}
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.functions.co.KeyedBroadcastProcessFunction
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.connectors.kafka.{FlinkKafkaConsumer, FlinkKafkaProducer}
+import org.apache.flink.util.Collector
+
+import scala.collection.mutable
 /**
- * Skeleton for a Flink Streaming Job.
- *
- * For a tutorial how to write a Flink streaming application, check the
- * tutorials and examples on the <a href="http://flink.apache.org/docs/stable/">Flink Website</a>.
- *
- * To package your application into a JAR file for execution, run
- * 'mvn clean package' on the command line.
- *
- * If you change the name of the main class (with the public static void main(String[] args))
- * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
- */
+ *author : liyuan
+ *time : 2019/9/19 19:18
+ *desc : {kafka实时4G基站数据 与关系数据库中重点人库数据关联预警 ，采取广播状态方式}
+**/
 object StreamingJob {
   def main(args: Array[String]) {
     // set up the streaming execution environment
-    val env = StreamExecutionEnvironment.getExecutionEnvironment
-
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime)
+    env.setParallelism(1)
     /*
-     * Here, you can start creating your execution plan for Flink.
-     *
-     * Start with getting some data from the environment, like
-     *  env.readTextFile(textPath);
-     *
-     * then, transform the resulting DataStream[String] using operations
-     * like
-     *   .filter()
-     *   .flatMap()
-     *   .join()
-     *   .group()
-     *
-     * and many more.
-     * Have a look at the programming guide:
-     *
-     * http://flink.apache.org/docs/latest/apis/streaming/index.html
-     *
+      postgre FZ档案库数据
      */
+    val crime = env.addSource(new PostgreSource)
+      .map(c =>mutable.HashMap(c.num -> c))
+    val stateDescriptor =  new MapStateDescriptor[String,mutable.HashMap[String,CrimeDoc]](
+      "crime-state",
+      classOf[String],
+      //hashMap检索较快
+      classOf[mutable.HashMap[String,CrimeDoc]])
+    val crimeState = crime.broadcast(stateDescriptor)
+    /*
+      kafka基站实时数据
+     */
+    val stream = env
+      .addSource(createKafkaSource())
+      .map(line => {
+        var phone: Phone = Phone("None", "", 0.0f, 0.0f)
+        try {
+          phone = JSON.parseObject(line, classOf[Phone])
+        } catch {
+          case e: Exception => println("jsonSerializer Exception!" + e.getCause)
+        }
+        phone
+      })
+      .filter(_.num != "None")
+        .keyBy(_.num)
+        .timeWindow(Time.seconds(10))
+        .reduce((p1,p2)=>p1)
+        .connect(crimeState)
+        .process(broadStateProcessFunction(stateDescriptor))
+        //预警成功记录过滤
+        .filter(_.num!="None")
+        .map(w =>JSON.toJSONString(w,true)).setParallelism(10)
+
+    stream.print()
+    val myProducer = new FlinkKafkaProducer[String](
+      "192.168.1.110:9092",         // broker list
+      "warn-crime",               // target topic
+      new SimpleStringSchema)   // serialization schema
+
+    // versions 0.10+ allow attaching the records' event timestamp when writing them to Kafka;
+    // this method is not available for earlier Kafka versions
+    myProducer.setWriteTimestampToKafka(true)
+    stream.addSink(myProducer)
 
     // execute program
-    env.execute("Flink Streaming Scala API Skeleton")
+    env.execute("warn-crime")
+  }
+
+  /**
+   * author : liyuan
+   * time : 2019/9/19 17:30
+   * desc : {创建Kafka 数据源}
+   **/
+  def createKafkaSource() = {
+    val properties = new Properties()
+    properties.setProperty("bootstrap.servers", "192.168.1.110:9092")
+    properties.setProperty("group.id", "flink-group")
+    val kafka = new FlinkKafkaConsumer[String]("topic", new SimpleStringSchema(), properties)
+    kafka.setStartFromEarliest()
+    kafka
+  }
+  /**
+   *author : liyuan
+   *time : 2019/9/19 19:03
+   *desc : { 广播状态处理函数}
+  **/
+  def broadStateProcessFunction(descipter: MapStateDescriptor[String,mutable.HashMap[String,CrimeDoc]]) ={
+    val keyedBroadProcess = new KeyedBroadcastProcessFunction[String,Phone,mutable.HashMap[String,CrimeDoc],Warn] {
+      /*
+        与广播状态合并处理
+       */
+      override def processElement(in1: Phone,
+                                  readOnlyContext: KeyedBroadcastProcessFunction[String, Phone, mutable.HashMap[String,CrimeDoc], Warn]#ReadOnlyContext,
+                                  collector: Collector[Warn]): Unit = {
+        val crimeMap: mutable.HashMap[String, CrimeDoc] = readOnlyContext.getBroadcastState(descipter).get("crime")
+        val doc = crimeMap.getOrElse(in1.num,CrimeDoc(-1,"None","","",""))
+        if("None" != doc){
+          collector.collect(Warn(in1.num,doc.name,"2019-09-20",doc.detail))
+        }else{
+          collector.collect(Warn("None","","",""))
+        }
+      }
+      /*
+        读取广播状态
+       */
+      override def processBroadcastElement(in2: mutable.HashMap[String,CrimeDoc],
+                                           context: KeyedBroadcastProcessFunction[String, Phone, mutable.HashMap[String,CrimeDoc], Warn]#Context,
+                                           collector: Collector[Warn]): Unit = {
+        context.getBroadcastState(descipter).put("crime",in2)
+      }
+    }
+    keyedBroadProcess
   }
 }
